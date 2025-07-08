19edfc169f86f2820fd77f1987277172e8905077
Thundere <44729071+Thunder099@users.noreply.github.com>
2025-07-08 19:26:20 -0300

    Add files via upload

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,10 +1,10 @@
 function analise(num){
-    const input = document.getElementById("num");
-    input.value += num;
+    const input = document.getElementById("num"); /* pega a coordenada do input pelo id */
+    input.value += num; /* isso aqui é pra adicionar o numero e o simbolo na mesma coisa, e dentro do input tambem */
 }
 
 function calcular() {
-    const input = document.getElementById("num");
-    const resultado = eval(input.value);
-    input.value = resultado;
+    const input = document.getElementById("num"); /* novamente pega o input pelo id */
+    const resultado = eval(input.value); /* esse eval serve pra rodar qualquer coisa que estiver no valor do input como se fosse código, ou seja, se voce escrever alert("cavalo") e apertar o "=" aparece um alert falando cavalo '-' */
+    input.value = resultado; /* isso coloca o valor da constante resultado no input */
 }
